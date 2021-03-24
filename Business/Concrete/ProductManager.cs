2d2afe2813207a2a6f69c096ff5a14d09989a513
Nasuf Mutlu <nasufmutlu@hotmail.com>
2021-03-24 23:46:03 +0300

    .

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -30,7 +30,7 @@ namespace Business.Concrete
         }
 
         //Claim
-        //[SecuredOperation("product.add,admin")]
+        [SecuredOperation("product.add")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
